7001163ea8f753a86d99ac90aa87090497f5c730
kfunnel <lsk7209@gmail.com>
2025-11-06 15:02:42 +0900

    fix: _worker.ts瑜?fetch handler ?뺤떇?쇰줈 ?섏젙

--- functions/_worker.ts
+++ functions/_worker.ts
@@ -2,11 +2,16 @@
  * Cloudflare Pages Functions Worker
  * 
  * Cloudflare Pages Functions는 _worker.ts 파일을 찾아서 사용합니다.
- * Hono 앱을 직접 export합니다.
+ * Hono 앱을 fetch handler로 export합니다.
  */
 
 import app from './index'
 
-// Hono 앱은 fetch 메서드를 가지고 있어서 Pages Functions에서 직접 사용 가능
-export default app
+// Cloudflare Pages Functions는 fetch handler를 기대합니다
+// Hono 앱은 fetch 메서드를 가지고 있어서 직접 사용 가능
+export default {
+  fetch: (request: Request, env: any, ctx: ExecutionContext) => {
+    return app.fetch(request, env, ctx)
+  }
+}
 
