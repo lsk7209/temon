f49e160603211ef916a6dc1c0f4d6a23e6ad1b04
kfunnel <lsk7209@gmail.com>
2025-11-06 14:52:04 +0900

    fix: Cloudflare Pages wrangler.toml ?ㅼ젙 理쒖쟻??諛?_worker.ts ?섏젙

--- functions/_worker.ts
+++ functions/_worker.ts
@@ -2,9 +2,11 @@
  * Cloudflare Pages Functions Worker
  * 
  * Cloudflare Pages Functions는 _worker.ts 파일을 찾아서 사용합니다.
- * 이 파일은 functions/index.ts의 Hono 앱을 re-export합니다.
+ * Hono 앱을 직접 export합니다.
  */
 
-// functions/index.ts에서 Hono 앱을 import하여 export
-export { default } from './index'
+import app from './index'
+
+// Hono 앱은 fetch 메서드를 가지고 있어서 Pages Functions에서 직접 사용 가능
+export default app
 
